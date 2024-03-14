0c985e9aaa09b1697ad5abf2e5d9f9f3ecd3a973
susthir <susthir19march@gmail.com>
2024-03-14 20:42:39 +0530

    color picker

--- Project_1/p1_script.js
+++ Project_1/p1_script.js
@@ -14,5 +14,17 @@ buttons.forEach(function(button){
         if(e.target.id == 'yellow'){
             body.style.backgroundColor = e.target.id;
         }
+        if(e.target.id == 'col1'){
+            body.style.backgroundColor = '#B5C0D0';
+        }
+        if(e.target.id == 'col2'){
+            body.style.backgroundColor = '#CCD3CA';
+        }
+        if(e.target.id == 'col3'){
+            body.style.backgroundColor = '#DC8686';
+        }
+        if(e.target.id == 'col4'){
+            body.style.backgroundColor = '#AAD7D9';
+        }
     })
 })
