95a43e89310290b97b0ca09cfcb996638f8ec3fa
susthir <susthir19march@gmail.com>
2024-04-02 21:40:26 +0530

    image to string

---
+++ Project_5/script.js
@@ -0,0 +1,18 @@
+let String = "";
+
+function imageUploaded() {
+  let file = document.querySelector("input[type=file]")["files"][0];
+  let reader = new FileReader();
+  reader.onload = function () {
+    String = reader.result.replace("data:", "").replace(/^.+,/, "");
+
+    imageStringsep = String;
+    // console.log(String);
+  };
+  reader.readAsDataURL(file);
+}
+
+function displayString() {
+  //onsole.log("String about to be printed");
+  alert(String);
+}
